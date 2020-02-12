96f3d3d7bf95724d76cc48acdd4cbe8680792ef7
mucteb <31360743+mucteb@users.noreply.github.com>
2020-02-12 11:15:16 -0500

    Update DemoGithub.java
    // added sub method, Muharrem KAYA, 2020

--- src/demogithub/DemoGithub.java
+++ src/demogithub/DemoGithub.java
@@ -2,13 +2,14 @@
 package demogithub;
 
 /* author Muharrem KAYA */
+// added sub method, Muharrem KAYA, 2020
 
   public class DemoGithub 
   {
       public static void main(String[] args) 
       {
-          System.out.println(mySum(5,3));       
- 
+          System.out.println("Sum: "+mySum(5,3));       
+          System.out.println("Sub: "+mySub(5,3));   
       } 
       
       public static double mySum(double a, double b)
@@ -16,5 +17,11 @@ package demogithub;
           double summ=a+b;
           return summ;
       }
+    
+     public static double mySub(double a, double b)
+       {
+          double sub=a-b;
+          return sub;
+      }      
       
   }
