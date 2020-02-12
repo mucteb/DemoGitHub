de9719ec4b59ad178603cb7e2dea9fa367e5ea7c
mucteb <mucteb@gmail.com>
2020-02-12 11:21:18 -0500

    Added author names in the Deliverable 1
     * @author Muharrem Kaya, 2020
    Added multiple method

--- src/demogithub/DemoGithub.java
+++ src/demogithub/DemoGithub.java
@@ -3,6 +3,7 @@ package demogithub;
 
 /* author Muharrem KAYA */
 // added sub method, Muharrem KAYA, 2020
+// added mul method, Derya Kaya, 12 Feb, 2020
 
   public class DemoGithub 
   {
@@ -10,6 +11,7 @@ package demogithub;
       {
           System.out.println("Sum: "+mySum(5,3));       
           System.out.println("Sub: "+mySub(5,3));   
+          System.out.println("Mul: "+myMul(5,3));
       } 
       
       public static double mySum(double a, double b)
@@ -22,6 +24,11 @@ package demogithub;
        {
           double sub=a-b;
           return sub;
+      }      
+     public static double myMul(double a, double b)
+       {
+          double mul=a*b;
+          return mul;
       }      
       
   }
