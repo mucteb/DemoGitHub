681bf660819b3d6453a5b475ff157a124bda2736
mucteb <mucteb@gmail.com>
2020-02-12 11:03:25 -0500

    Added author names in the Deliverable 1
     * @author Muharrem Kaya, 2020

---
+++ src/demogithub/DemoGithub.java
@@ -0,0 +1,20 @@
+
+package demogithub;
+
+/* author Muharrem KAYA */
+
+  public class DemoGithub 
+  {
+      public static void main(String[] args) 
+      {
+          System.out.println(mySum(5,3));       
+ 
+      } 
+      
+      public static double mySum(double a, double b)
+      {
+          double summ=a+b;
+          return summ;
+      }
+      
+  }
